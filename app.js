c8a14f27e9672f96046bf07fde64c458c16048ef
softech-solutions <softech.soluciones@gmail.com>
2022-01-07 20:11:54 -0500

    Adding sum to case 2

--- app.js
+++ app.js
@@ -6,7 +6,10 @@ function menu(opcion) {
             console.log('Esta es mi opcion => 1');
             break;
         case 2:
-            console.log('Esta es mi opcion => 2');
+            console.log('Aqui deberia ir una opcion => 2');
+            let a = 10;
+            let b = 20;
+            console.log(`La suma de ${a} + ${b} es: ${a + b}`);
             break;
         case 3:
             console.log('Esta es mi opcion => 3');
