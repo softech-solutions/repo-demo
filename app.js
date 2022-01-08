d65fba02494b7b5729c6639d663e2d41a726fb6a
softech-solutions <softech.soluciones@gmail.com>
2022-01-07 20:08:43 -0500

    Adding app.js to repository

---
+++ app.js
@@ -0,0 +1,17 @@
+console.log('This is my first repo on git');
+
+function menu(opcion) {
+    switch (opcion) {
+        case 1:
+            console.log('Esta es mi opcion => 1');
+            break;
+        case 2:
+            console.log('Esta es mi opcion => 2');
+            break;
+        case 3:
+            console.log('Esta es mi opcion => 3');
+            break;
+    }
+}
+
+menu(1);
