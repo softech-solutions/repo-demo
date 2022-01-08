6d07159659ca221e467b61338fc74a4434582f10
softech-solutions <softech.soluciones@gmail.com>
2022-01-07 20:23:28 -0500

    Revert "Adding sum to case 2"
    This reverts commit c8a14f27e9672f96046bf07fde64c458c16048ef.

--- app.js
+++ app.js
@@ -6,10 +6,7 @@ function menu(opcion) {
             console.log('Esta es mi opcion => 1');
             break;
         case 2:
-            console.log('Aqui deberia ir una opcion => 2');
-            let a = 10;
-            let b = 20;
-            console.log(`La suma de ${a} + ${b} es: ${a + b}`);
+            console.log('Esta es mi opcion => 2');
             break;
         case 3:
             console.log('Esta es mi opcion => 3');
